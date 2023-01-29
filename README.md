323ccdceddb0507e4ec2161cdab60e9f59a5f1eb
ntthang123@gmail.com <ntthang123@gmail.com>
2023-01-29 18:49:39 -0500

    edit script.py

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # streamlit-app
+
+
+### How to install streamlit
+https://docs.streamlit.io/library/get-started/installation
--- script.py
+++ script.py
@@ -1,2 +1,31 @@
-import streamlit
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+
+st.write("""
+# My first app
+
+Hello *World!*
+
+"""
+)
+
+st.title("this is a title")
+
+st.header("This is a header")
+
+st.write("this is  a regular text")
+
+dictionary_sample = {
+    "key":1,
+    "name": "Thomas"
+}
+
+st.write(dictionary_sample)
+
+st.sidebar.write("Sidebar goes here:")
+
+df = pd.DataFrame(np.random.randn(50,20), columns = ('col %d' % i for i in range(20)))
+
+st.dataframe(df)
